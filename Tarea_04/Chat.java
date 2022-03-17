36c2f2f68becf91cee725e7a8bcc616da38588c7
KarinaRmzG <kramirezg1400@alumno.ipn.mx>
2022-03-17 01:02:10 -0600

    Chat v1.1

--- Tarea_04/Chat.java
+++ Tarea_04/Chat.java
@@ -1,3 +1,9 @@
+/***
+ * Tarea 04:        Chat Multicast
+ * @author          Ramírez Galindo Karina
+ * Descripción:     Desarrollar un solo programa en Java en modo consola que implemente un chat 
+ *                  utilizando comunicación multicast mediante datagramas.
+ */
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.DatagramPacket;
@@ -14,13 +20,27 @@ class Chat {
     public static final String IP_MULTICAST = "230.0.0.0";
     public static final int MAX_BITS_DATAGRAM = 1024;
 
+    /**
+     * Función para enviar los mensajes multicast
+     * @param buffer    mensaje a enviar
+     * @param ip        grupo multicast
+     * @param puerto    puerto al que se va a transmitir
+     * @throws IOException
+     */
     static void envia_mensaje_multicast(byte[] buffer, String ip, int puerto) throws IOException {
-        System.out.println("Ingrese el mensaje a enviar:");
+        //System.out.println("Ingrese el mensaje a enviar:");
         DatagramSocket socket = new DatagramSocket();
         socket.send(new DatagramPacket(buffer, buffer.length, InetAddress.getByName(ip), puerto));
         socket.close();
     }
 
+    /**
+     * Función para recibir los mensajes multicast
+     * @param socket                multicast socket
+     * @param longitud_mensaje      
+     * @return
+     * @throws IOException
+     */
     static byte[] recibe_mensaje_multicast(MulticastSocket socket, int longitud_mensaje) throws IOException {
         byte[] buffer = new byte[longitud_mensaje];
         DatagramPacket paquete = new DatagramPacket(buffer, buffer.length);
@@ -42,8 +62,8 @@ class Chat {
                     SocketAddress grupo = new InetSocketAddress(dir,PUERTO);//grupo y puerto al que se va a transmitir (50000)
                     socket.joinGroup(grupo, ni);//reporte de membresia IGMP --> uniendose a la direccion 230.0.0.0 por la NIC seleccionada
                     byte[] buffer = recibe_mensaje_multicast(socket, MAX_BITS_DATAGRAM);
-                    PrintStream out = new PrintStream(System.out,true,"Cp850");
-                    out.print(new String(buffer)+"\n");
+                    PrintStream salida = new PrintStream(System.out,true,"ISO-8859-1");
+                    salida.print(new String(buffer)+"\n");
                     socket.leaveGroup(grupo, ni);
                     socket.close();
                 } catch (IOException e){
@@ -61,15 +81,17 @@ class Chat {
         Worker w = new Worker();
         w.start();
         String nombre = args[0];
-        Scanner scanner = new Scanner(System. in);
-        System.out.println("Servicio iniciado.. comienza envio de anuncios");
-        // En un ciclo infinito se leerá cada mensaje del teclado y se enviará el
-        // mensaje al grupo 230.0.0.0 a través del puerto 50000.
-        while(true) {
+        try (Scanner scanner = new Scanner(System. in)) {
+            System.out.println("Servicio iniciado.. comienza envio de anuncios");
+            // En un ciclo infinito se leerá cada mensaje del teclado y se enviará el
+            // mensaje al grupo 230.0.0.0 a través del puerto 50000.
             //System.out.println("Ingrese el mensaje a enviar:");
-            String mensaje = scanner. nextLine();
-            byte buffer[] = String.format("%s dice %s", nombre, mensaje).getBytes();
-            envia_mensaje_multicast(buffer, IP_MULTICAST,PUERTO);
+            while(true) {
+                System.out.println("Ingrese el mensaje a enviar:");
+                String mensaje = scanner. nextLine();
+                byte buffer[] = String.format("%s dice %s", nombre, mensaje).getBytes();
+                envia_mensaje_multicast(buffer, IP_MULTICAST,PUERTO);
+            }
         }
     }
 
