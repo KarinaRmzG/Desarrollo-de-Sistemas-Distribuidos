b41f40b5226ab06ed4095deb5bcd5827ca96f888
KarinaRmzG <kramirezg1400@alumno.ipn.mx>
2022-05-05 20:18:11 -0500

    prueba

---
+++ PruebaCarrito/Cliente/js/scripts.js
@@ -0,0 +1,24 @@
+var foto = null;  // por default la foto es nula
+function get(id)
+			{
+				return document.getElementById(id);
+			}
+            function readSingleFile(files,imagen)
+			{
+				var file = files[0];
+				if (!file) return;
+				var reader = new FileReader();
+				reader.onload = function(e)
+				{
+					imagen.src = reader.result;
+					// reader.result incluye al principio: "data:image/jpeg;base64,"
+					foto = reader.result.split(',')[1];
+				};
+				reader.readAsDataURL(file);
+			}
+function quita_foto()
+			{
+				foto=null;
+				get('consulta_imagen').src='usuario_sin_foto.png';
+				get('consulta_file').value='';
+			}
